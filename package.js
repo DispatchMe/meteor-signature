d5a5d3b29be5458340baf553ef4f6f0f84941ab7
Morten Henriksen <mh@gi2.dk>
2016-01-22 22:50:05 -0500

    published 1.0.2-rc.1
    This solves an issue with Meteor@1.3-modules-beta.4 where recursive
    dependencies would occur

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'dispatch:signature',
   summary: 'A signature component for meteor (also works with on cordova).',
-  version: '1.0.1',
+  version: '1.0.2-rc.1',
   git: 'https://github.com/DispatchMe/meteor-signatures'
-});
-
-Npm.depends({
-  'signature_pad': '1.5.1'
 });
 
 Package.onUse(function (api) {
@@ -22,7 +18,7 @@ Package.onUse(function (api) {
 
   api.addFiles([
     // library
-    '.npm/package/node_modules/signature_pad/signature_pad.min.js',
+    'vendor/signature_pad.min.js',
 
     // package
     'signature.js',
